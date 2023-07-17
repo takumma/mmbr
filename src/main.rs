c9c49196966333acca84605a01dcd4c10a4e8db6
takumma <taku.3001@icloud.com>
2023-07-17 21:46:55 +0900

    add tokenizer

--- src/main.rs
+++ src/main.rs
@@ -26,3 +26,58 @@ fn build_ui(app: &Application) {
 
     window.present();
 }
+
+pub enum State {
+    Data,
+}
+
+pub enum HtmlToken {
+    Char(char),
+    Eof,
+}
+
+pub struct HtmlTokenizer {
+    input: Vec<char>,
+    state: State,
+    pos: usize,
+}
+
+impl HtmlTokenizer {
+    pub fn new(html: String) -> Self {
+        Self {
+            state: State::Data,
+            pos: 0,
+            input: html.chars().collect(),
+        }
+    }
+
+    fn is_eof(&self) -> bool {
+        self.pos > self.input.len()
+    }
+}
+
+impl Iterator for HtmlTokenizer {
+    type Item = HtmlToken;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.is_eof() {
+            return None;
+        };
+
+        loop {
+            let c = self.input[self.pos];
+            self.pos += 1;
+
+            match self.state {
+                State::Data => match c {
+                    _ => {
+                        if self.is_eof() {
+                            return Some(HtmlToken::Eof);
+                        }
+                        return Some(HtmlToken::Char(c));
+                    }
+                },
+            }
+        }
+    }
+}
